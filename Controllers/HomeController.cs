c5cfd944bce41818edd8c0ee129bc42736c98a42
Casey Gower <caseyg@charter.net>
2017-03-08 23:39:04 -0600

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NumberFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Results(string number, string color)
+        {
+            int n;
+            if ( int.TryParse(number, out n) )
+            {
+                return View(n);
+            }else
+            {
+                return Error();
+            }  
+        }
+        
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
